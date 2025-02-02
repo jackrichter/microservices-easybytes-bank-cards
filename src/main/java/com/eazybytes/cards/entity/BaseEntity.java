dac921335cb10f4945191d384ad9b7e56ea0aac7
jackrichter <jackrichter1@gmail.com>
2025-02-02 14:24:28 +0100

    Auditing of the Base-Entity fields enabled.

--- src/main/java/com/eazybytes/cards/entity/BaseEntity.java
+++ src/main/java/com/eazybytes/cards/entity/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.eazybytes.cards.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,10 +9,12 @@ import lombok.ToString;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @Getter @Setter @ToString
 public class BaseEntity {
 
